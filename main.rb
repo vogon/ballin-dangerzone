e7f02b9abade627a79a0d3d6d67c33b03b9fcf80
Colin Bayer <vogon@icculus.org>
2012-04-21 12:23:31 -0700

    god damn it hard tabs how did you get in here

--- main.rb
+++ main.rb
@@ -3,36 +3,36 @@
 require 'rubygame'
 
 class Game
-	def initialize
-		@screen = Rubygame::Screen.new [640,480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
-		@screen.title = "project ballin' dangerzone"
+    def initialize
+        @screen = Rubygame::Screen.new [640,480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+        @screen.title = "project ballin' dangerzone"
  
-		@queue = Rubygame::EventQueue.new
-		@clock = Rubygame::Clock.new
-		@clock.target_framerate = 60
-	end
+        @queue = Rubygame::EventQueue.new
+        @clock = Rubygame::Clock.new
+        @clock.target_framerate = 60
+    end
  
-	def run
-		loop do
-			update
-			draw
-			@clock.tick
-		end
-	end
+    def run
+        loop do
+            update
+            draw
+            @clock.tick
+        end
+    end
  
-	def update
-		@queue.each do |ev|
-			case ev
-				when Rubygame::QuitEvent
-					Rubygame.quit
-					exit
-			end
-		end
-	end
+    def update
+        @queue.each do |ev|
+            case ev
+                when Rubygame::QuitEvent
+                    Rubygame.quit
+                    exit
+            end
+        end
+    end
  
-	def draw
-		@screen.flip
-	end
+    def draw
+        @screen.flip
+    end
 end
  
 game = Game.new
