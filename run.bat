a4c69c42192d013af8133cff5672e997a1227d5a
Colin Bayer <vogon@icculus.org>
2012-04-21 11:38:07 -0700

    added prebuilt libs for win32 as well as magic to make run.bat use them

--- run.bat
+++ run.bat
@@ -3,4 +3,5 @@
 echo ===== looking up google maps to the danger zone =====
 call bundle install
 echo ===== pullin' out my driveway to the danger zone =====
+set PATH=%PATH%;%CD%\lib-win32
 bundle exec ruby main.rb
